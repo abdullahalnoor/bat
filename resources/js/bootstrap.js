f818c2f259ae39074a5e71bbbfe524aa9ece4f31
abdullahalnoor <aalnoor74@gmail.com>
2019-09-30 12:19:56 +0600

    first commit

---
+++ resources/js/bootstrap.js
@@ -0,0 +1,121 @@
+window._ = require('lodash');
+
+/**
+ * We'll load the axios HTTP library which allows us to easily issue requests
+ * to our Laravel back-end. This library automatically handles sending the
+ * CSRF token as a header based on the value of the "XSRF" token cookie.
+ */
+
+try {
+    window.Popper = require('popper.js').default;
+    window.$ = window.jQuery = require('jquery');
+    feather = require('feather-icons');
+    feather.replace();
+    
+    require('bootstrap');
+
+} catch (e) {}
+
+
+
+window.window.jQuery.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+/**
+ * Next we will register the CSRF Token as a common header with Axios so that
+ * all outgoing HTTP requests automatically have it attached. This is just
+ * a simple convenience so we don't have to attach every token manually.
+ */
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.window.jQuery.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
+
+try {
+  
+   
+ 
+
+} catch (e) {}
+
+
+/**
+ * Echo exposes an expressive API for subscribing to channels and listening
+ * for events that are broadcast by Laravel. Echo and event broadcasting
+ * allows your team to easily build robust real-time web applications.
+ */
+
+// import Echo from 'laravel-echo'
+
+// window.Pusher = require('pusher-js');
+
+// window.Echo = new Echo({
+//     broadcaster: 'pusher',
+//     key: process.env.MIX_PUSHER_APP_KEY,
+//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+//     encrypted: true
+// });
+
+// theme jquery
+
+(function () {
+    $(window).scroll(function () {
+        var top = $(document).scrollTop();
+        if (top > 50)
+            $('#home > .navbar').removeClass('navbar-transparent');
+        else
+            $('#home > .navbar').addClass('navbar-transparent');
+    });
+
+    $("a[href='#']").click(function (e) {
+        e.preventDefault();
+    });
+
+    $('.bs-component [data-toggle="popover"]').popover();
+    $('.bs-component [data-toggle="tooltip"]').tooltip();
+    $(".bs-component").each(function () {
+        var $button = $("<button class='source-button btn btn-primary btn-xs' role='button' tabindex='0'>&lt; &gt;</button>");
+        $(this).append($button);
+    });
+
+    $('body').on('click', '.source-button', function (e) {
+        e.preventDefault();
+
+        var html = $(this).parent().html();
+        html = cleanSource(html);
+        $("#source-modal pre").text(html);
+        $("#source-modal").modal();
+    });
+
+    function cleanSource(html) {
+        html = html.replace(/×/g, "&times;")
+            .replace(/«/g, "&laquo;")
+            .replace(/»/g, "&raquo;")
+            .replace(/←/g, "&larr;")
+            .replace(/→/g, "&rarr;");
+
+        var lines = html.split(/\n/);
+
+        lines.shift();
+        lines.splice(-1, 1);
+
+        var indentSize = lines[0].length - lines[0].trim().length,
+            re = new RegExp(" {" + indentSize + "}");
+
+        lines = lines.map(function (line) {
+            if (line.match(re)) {
+                line = line.substring(indentSize);
+            }
+
+            return line;
+        });
+
+        lines = lines.join("\n");
+
+        return lines;
+    }
+
+})();
